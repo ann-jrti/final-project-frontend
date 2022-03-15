9b5df90869e9eff2ecf9871c70c602cf39a034e6
ann <asog.jrti@gmail.com>
2022-03-15 18:20:34 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Btn from './components/btn';
+
+
+function App() {
+  return (
+    <div className="App">
+      <p>working</p>
+      <Btn></Btn>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/btn/index.jsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
+import styled from 'styled-components'
+import { css } from '@emotion/react';
+import { Switch } from '@mui/material';
+
+const Buttonn = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+
+  ${props =>
+        props.primary &&
+        css`
+      background: palevioletred;
+      color: white;
+    `};
+`
+
+const SwitchStyled = styled(Switch)({
+    color: 'red',
+})
+
+const MyComponent = styled('div')({
+    color: 'darkslategray',
+    backgroundColor: 'aliceblue',
+    padding: 8,
+    borderRadius: 4,
+});
+
+const MyComponentt = styled(Button)({
+    color: 'darkslategray'
+});
+
+export default function Btn() {
+    return (
+        <>
+            <Button variant="contained">Hello World</Button>
+            <Buttonn>Boton</Buttonn>
+            <MyComponent>Styled div</MyComponent>
+            <MyComponentt>hds</MyComponentt>
+            <SwitchStyled></SwitchStyled>
+        </>
+
+    )
+}
