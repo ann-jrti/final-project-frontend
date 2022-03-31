214012f2b1000e0e400a6f12675605280b83a279
ann <asog.jrti@gmail.com>
2022-03-31 10:59:05 +0100

    fix dark theme bugs

--- src/pages/playerResultsSearch/info-player-card/InfoPlayerCard.jsx
+++ src/pages/playerResultsSearch/info-player-card/InfoPlayerCard.jsx
@@ -1,120 +1,185 @@
 import * as React from 'react';
-import { Card, CardContent, Grid, Box, CardMedia, Divider, Typography, CardActionArea } from '@mui/material';
+import {
+  Card,
+  CardContent,
+  Grid,
+  Box,
+  CardMedia,
+  Divider,
+  Typography,
+  CardActionArea,
+} from '@mui/material';
 import styled from '@emotion/styled';
 import provisionalIcon from '../../../assets/tier-icons/bronze-icon.png';
 import silverIcon from '../../../assets/tier-icons/silver-icon.png';
 import goldIcon from '../../../assets/tier-icons/gold-icon.png';
 import platIcon from '../../../assets/tier-icons/platinum-icon.png';
 import diamondIcon from '../../../assets/tier-icons/diamond-icon.png';
-import challengerIcon from '../../../assets/tier-icons/challenger-icon.png';
+import grandmasterIcon from '../../../assets/tier-icons/challenger-icon.png';
 import masterIcon from '../../../assets/tier-icons/master-icon.png';
-import level50 from '../../../assets/level-icons/level_50-74.png'
+import level50 from '../../../assets/level-icons/level_50-74.png';
 
 const StyleCard = styled(Card)`
-max-width: 200rem;
-margin: 2rem;
-
-`
+  max-width: 200rem;
+  margin: 2rem;
+`;
 
 export default function InfoPlayerCard(props) {
+  const tierIcons = {
+    PROVISIONAL: provisionalIcon,
+    SILVER: silverIcon,
+    GOLD: goldIcon,
+    PLATINUM: platIcon,
+    DIAMOND: diamondIcon,
+    GRANDMASTER: grandmasterIcon,
+    MASTER: masterIcon,
+  };
 
-    const tierIcons = {
-        'PROVISIONAL': provisionalIcon,
-        'SILVER': silverIcon,
-        'GOLD': goldIcon,
-        'PLATINUM': platIcon,
-        'DIAMOND': diamondIcon,
-        'CHALLENGER': challengerIcon,
-        'MASTER': masterIcon
-    }
+  return (
+    <>
+      {/* <Grid container display={'flex'} justifyContent={'center'}> */}
+      <StyleCard>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="300"
+            image={props.image}
+            alt="green iguana"
+          />
 
+          <CardContent>
+            <Box display={'flex'} flexDirection={'column'} gap={1}>
+              <Box
+                display={'flex'}
+                justifyContent={'center'}
+                flexDirection="column"
+                alignItems="center"
+              >
+                <img
+                  style={{
+                    maxWidth: '4.5rem',
+                    maxHeight: '4.5rem',
+                    borderRadius: '50%',
+                    position: 'absolute',
+                    marginBottom: '7rem',
+                    border: '.3rem solid #8d99ae',
+                    backgroundColor: '#8d99ae',
+                  }}
+                  className="search-player-avatar-icon"
+                  src={props.imgSrc}
+                ></img>
 
+                <Typography
+                  paddingTop={2}
+                  marginBottom={1}
+                  gutterBottom
+                  variant="h3"
+                  component="div"
+                  color="black"
+                >
+                  {props.name}
+                </Typography>
+              </Box>
+              <Divider sx={{ marginBottom: '.8rem' }} />
 
-    return (
-        <>
-            {/* <Grid container display={'flex'} justifyContent={'center'}> */}
-            <StyleCard  >
-                <CardActionArea>
-                    <CardMedia
-                        component="img"
-                        height="300"
-                        image={props.image}
-                        alt="green iguana"
-                    />
+              <Box
+                display="flex"
+                gap={4}
+                flexDirection="row"
+                justifyContent="center"
+                marginTop={-1}
+              >
+                <Box
+                  display={'flex'}
+                  flexDirection="column"
+                  justifyContent="center"
+                  alignItems={'center'}
+                >
+                  {/* <BadgeOutlined></BadgeOutlined> */}
+                  <Box>
+                    <img width={90} src={level50}></img>
+                  </Box>
+                  <Typography variant="body2" color="black">
+                    Summoner level
+                  </Typography>
+                  <Typography color="primary" variant="h5">
+                    {props.level}
+                  </Typography>
+                </Box>
 
-                    <CardContent >
+                <Box
+                  display={'flex'}
+                  gap={1}
+                  flexDirection="column"
+                  justifyContent="center"
+                  marginBottom={0.2}
+                  alignItems={'center'}
+                >
+                  <Box marginBottom={-2}>
+                    <img width={100} src={tierIcons[props.rankIcon]}></img>
+                  </Box>
+                  <Box
+                    display="flex"
+                    flexDirection="column"
+                    alignItems="center"
+                  >
+                    <Typography variant="body2" color="black">
+                      {props.queue}
+                    </Typography>
+                    <Typography color="primary" variant="h5">
+                      {props.rank}
+                    </Typography>
+                  </Box>
+                </Box>
+              </Box>
+              <Divider />
 
-                        <Box display={'flex'} flexDirection={'column'} gap={1}>
-                            <Box display={'flex'} justifyContent={'center'} flexDirection='column' alignItems='center' >
+              <Box display={'flex'} flexDirection={'column'} gap={2}>
+                <Box display={'flex'} flexDirection="column" gap={2}>
+                  <Box display="flex" justifyContent="center">
+                    <Typography color="primary" variant={'h5'}>
+                      This season
+                    </Typography>
+                  </Box>
+                </Box>
 
-                                <img style={{ maxWidth: '4.5rem', maxHeight: '4.5rem', borderRadius: '50%', position: 'absolute', marginBottom: '7rem', border: '.3rem solid #8d99ae', backgroundColor: '#8d99ae' }} className='search-player-avatar-icon' src={props.imgSrc}></img>
+                <Box display="flex" justifyContent="center" gap={3}>
+                  <Box
+                    display="flex"
+                    gap={4}
+                    flexDirection="row"
+                    justifyContent="center"
+                    alignItems="center"
+                  >
+                    {/* <Typography variant={'h4'}>Queue Type: {props.queue}</Typography> */}
+                    <Box display={'flex'} gap={1} alignItems={'center'}>
+                      {/* <ThumbUpOutlinedIcon></ThumbUpOutlinedIcon> */}
+                      <Typography variant="body2" color="primary">
+                        Wins: {props.wins}
+                      </Typography>
+                    </Box>
 
-                                <Typography paddingTop={2} marginBottom={1} gutterBottom variant="h3" component="div">{props.name}</Typography>
+                    <Box display={'flex'} gap={1} alignItems={'center'}>
+                      {/* <ThumbDownOutlinedIcon></ThumbDownOutlinedIcon> */}
+                      <Typography variant="body2" color="primary">
+                        Losses: {props.losses}{' '}
+                      </Typography>
+                    </Box>
 
-                            </Box>
-                            <Divider sx={{ marginBottom: '.8rem' }} />
-
-
-                            <Box display='flex' gap={4} flexDirection='row' justifyContent='center' marginTop={-1}>
-                                <Box display={'flex'} flexDirection='column' justifyContent='center' alignItems={'center'}>
-                                    {/* <BadgeOutlined></BadgeOutlined> */}
-                                    <Box >
-                                        <img width={90} src={level50}></img>
-                                    </Box>
-                                    <Typography variant="body2" color="text.secondary">Summoner level</Typography>
-                                    <Typography variant='h5'>{props.level}</Typography>
-
-                                </Box>
-
-                                <Box display={'flex'} gap={1} flexDirection='column' justifyContent='center' marginBottom={.2} alignItems={'center'}>
-                                    <Box marginBottom={-2}>
-                                        <img width={100} src={tierIcons[props.rankIcon]}></img>
-                                    </Box>
-                                    <Box display='flex' flexDirection='column' alignItems='center'>
-                                        <Typography variant="body2" color="text.secondary">{props.queue}</Typography>
-                                        <Typography variant='h5'>{props.rank}</Typography>
-                                    </Box>
-                                </Box>
-
-                            </Box>
-                            <Divider />
-
-                            <Box display={'flex'} flexDirection={'column'} gap={2}>
-
-                                <Box display={'flex'} flexDirection='column' gap={2}>
-                                    <Box display='flex' justifyContent='center' >
-                                        <Typography variant={'h5'}>This season</Typography>
-                                    </Box>
-                                </Box>
-
-                                <Box display='flex' justifyContent='center' gap={3}>
-                                    <Box display='flex' gap={4} flexDirection='row' justifyContent='center' alignItems='center'>
-                                        {/* <Typography variant={'h4'}>Queue Type: {props.queue}</Typography> */}
-                                        <Box display={'flex'} gap={1} alignItems={'center'}>
-                                            {/* <ThumbUpOutlinedIcon></ThumbUpOutlinedIcon> */}
-                                            <Typography variant="body2" color="text.secondary">Wins: {props.wins}</Typography>
-                                        </Box>
-
-                                        <Box display={'flex'} gap={1} alignItems={'center'}>
-                                            {/* <ThumbDownOutlinedIcon></ThumbDownOutlinedIcon> */}
-                                            <Typography variant="body2" color="text.secondary">Losses: {props.losses} </Typography>
-                                        </Box>
-
-
-                                        <Box display={'flex'} gap={1} alignItems={'center'}>
-                                            {/* <StarBorder></StarBorder> */}
-                                            <Typography variant="body2" color="text.secondary">{props.playing}</Typography>
-                                        </Box>
-                                    </Box>
-                                </Box>
-                            </Box>
-                        </Box>
-                    </CardContent>
-                </CardActionArea>
-            </StyleCard>
-            {/* </Grid> */}
-        </>
-
-
-    );
+                    <Box display={'flex'} gap={1} alignItems={'center'}>
+                      {/* <StarBorder></StarBorder> */}
+                      <Typography variant="body2" color="primary">
+                        {props.playing}
+                      </Typography>
+                    </Box>
+                  </Box>
+                </Box>
+              </Box>
+            </Box>
+          </CardContent>
+        </CardActionArea>
+      </StyleCard>
+      {/* </Grid> */}
+    </>
+  );
 }
